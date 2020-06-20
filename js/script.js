c02d6fb923afb7148e6533875d1a5d77f45d803b
Filimon Mikhail <misafilimon255@mail.ru>
2020-06-20 19:37:20 +0300

    fix(js/script.js)fixed task completion

--- js/script.js
+++ js/script.js
@@ -5,12 +5,17 @@ const   books = document.querySelector('.books'),
         body = document.querySelector('body'),
         h2 = document.querySelectorAll('h2'),
         adv = document.querySelector('.adv'),
-        li = document.querySelectorAll('li');
+        li0 = ul[0].querySelectorAll('li'),
+        li1 = ul[1].querySelectorAll('li'),
+        li2 = ul[2].querySelectorAll('li'),
+        li3 = ul[3].querySelectorAll('li'),
+        li4 = ul[4].querySelectorAll('li'),
+        li5 = ul[5].querySelectorAll('li');
 
-        
 body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
 adv.remove();
 
+ul[3].prepend(li3[1])
 
 books.append(book[1]);
 books.append(book[0]);
@@ -24,30 +29,30 @@ h2[4].innerHTML = `<a href="https://github.com/azat-io/you-dont-know-js-ru/blob/
                target="_blank">Книга 3. this и Прототипы Объектов</a></h2>`;
 
 
-ul[0].prepend(li[9]);
-ul[0].prepend(li[7]);
-ul[0].prepend(li[5]);
-ul[0].prepend(li[4]);
-ul[0].prepend(li[8]);
-ul[0].prepend(li[6]);
-ul[0].prepend(li[3]);
-ul[0].prepend(li[1]);
-ul[0].prepend(li[0]);
+ul[0].prepend(li0[9]);
+ul[0].prepend(li0[7]);
+ul[0].prepend(li0[5]);
+ul[0].prepend(li0[4]);
+ul[0].prepend(li0[8]);
+ul[0].prepend(li0[6]);
+ul[0].prepend(li0[3]);
+ul[0].prepend(li0[1]);
+ul[0].prepend(li0[0]);
 
 
-ul[5].prepend(li[56]);
-ul[5].prepend(li[54]);
-ul[5].prepend(li[51]);
-ul[5].prepend(li[53]);
-ul[5].prepend(li[52]);
-ul[5].prepend(li[48]);
-ul[5].prepend(li[50]);
-ul[5].prepend(li[49]);
-ul[5].prepend(li[55]);
-ul[5].prepend(li[47]);
-ul[5].prepend(li[46]);
+ul[5].prepend(li5[10]);
+ul[5].prepend(li5[8]);
+ul[5].prepend(li5[5]);
+ul[5].prepend(li5[7]);
+ul[5].prepend(li5[6]);
+ul[5].prepend(li5[2]);
+ul[5].prepend(li5[4]);
+ul[5].prepend(li5[3]);
+ul[5].prepend(li5[9]);
+ul[5].prepend(li5[1]);
+ul[5].prepend(li5[0]);
 
 
 ul[2].insertAdjacentHTML('beforeend', `<li>Глава 8: За пределами ES6</li>`);
-ul[2].append(li[26]);
+ul[2].append(li2[9]);
 
